7b7d93e9cc9fc3c439b14f0af88e96f2267c13ab
suadtalafha <suadhusam@yahoo.com>
2021-06-13 16:32:18 +0300

    save

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function openNav() {
+    document.getElementById("myNav").style.width = "40%";
+  }
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+  
